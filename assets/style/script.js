df8fe17a99065e9cbc94acac8a504d9a0393570b
ferdinalaxe <ferdinalraihan@gmail.com>
2022-10-05 11:50:55 +0700

    fix bug in small viewport

--- assets/style/script.js
+++ assets/style/script.js
@@ -1,8 +1,88 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
+        stagePadding: 50,
         autoWidth: true,
-        items: 5,
+        lazyLoad:true,
         dots: true,
         margin: 10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+    });
+
+    $(".navbar__link").off('click').on('click', function(){
+        $(".navbar__link").removeClass("active");
+        $(this).addClass("active")
+    });
+
+    $(".beranda__image-group").attr("data-scroll", "show");
+    $(".beranda__text-group").children().each(function(i){
+        setTimeout(() => {
+            $(".beranda__text-group").children().eq(i).attr("data-scroll", "show")
+            if (i == 2) {
+                setTimeout(() => {
+                    $(".layanan__box-group").attr("data-scroll", "show");
+                    $(".layanan__box").each(function(i){
+                        setTimeout(() => {
+                            $(".layanan__box").eq(i).attr("data-scroll", "show")
+                        }, (i+1) * 150);
+                    })
+                }, 200);
+            }
+        }, (i+1) * 200);
+    });
+
+    $(".menu-button").click(function(e){
+        e.preventDefault();
+
+        $(".navbar ul").toggleClass("show")
     })
+
+})
+
+$(window).on('scroll', function(){
+    let wScroll = $(this).scrollTop();
+    
+    if (wScroll < 200) {
+        $(".navbar__link").removeClass("active");
+        $(".navbar__link[href='#beranda']").addClass("active")
+    }
+    
+    if (wScroll > $("#layanan").offset().top - 200) {
+        $("#layanan").find(".section-title").attr("data-scroll", "show")
+        $(".navbar__link").removeClass("active");
+        $(".navbar__link[href='#layanan']").addClass("active")    
+        $(".layanan__content-box").each(function(i){
+            setTimeout(() => {
+                $(".layanan__content-box").eq(i).attr("data-scroll", "show")
+            }, i * 200);
+        })
+    }
+    
+    if (wScroll > $("#produk").offset().top - 200) {
+        $(".navbar__link").removeClass("active");
+        $(".navbar__link[href='#produk']").addClass("active");
+        $("#produk .section-header").children().each(function(i){
+            setTimeout(() => {
+                $("#produk .section-header").children().eq(i).attr("data-scroll", "show")
+            }, i * 150);
+        });
+        
+        $(".produk__box").each(function(i){
+            setTimeout(() => {
+                $(".produk__box").eq(i).attr("data-scroll", "show")
+            }, (i+1) * 150);
+        });
+    }
+    
+    if (wScroll > $("#produk").offset().top + 100) {
+        $("#cara-pemesanan").find(".section-title").attr("data-scroll", "show")
+        $(".navbar__link").removeClass("active");
+        $(".navbar__link[href='#cara-pemesanan']").addClass("active");
+        $(".order__box").each(function(i){
+            setTimeout(() => {
+                $(".order__box").eq(i).attr("data-scroll", "show")
+            }, (i+1) * 150);
+        });
+    }
 })
