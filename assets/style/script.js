7eea5875e9932e72356ef61cb2e3812d9f26e712
ferdinalaxe <ferdinalraihan@gmail.com>
2022-10-05 09:50:23 +0700

    ganti tema

--- assets/style/script.js
+++ assets/style/script.js
@@ -1,20 +1,8 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         autoWidth: true,
-        items: 8,
+        items: 5,
         dots: true,
         margin: 10,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:8,
-                dots: true
-            }
-        }
     })
 })
